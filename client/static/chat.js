39ebc1cb166c5d385d283fe654d7259e6ddacafa
izzydotexe <israel.aristide@dawsoncollege.qc.ca>
2024-03-29 10:04:24 -0400

    move init of socket to chat lib

--- client/static/chat.js
+++ client/static/chat.js
@@ -1,5 +1,21 @@
 const _chatListeners = [];
-const socket = io();
+let socket;
+
+function initSocket() {
+    socket = io();
+
+    socket.connect()
+
+    socket.on('connect', () => {
+        console.log('Connected as: ' + socket.id)
+        socket.on('chat', (data) => {
+            const json = JSON.parse(data)
+            for (let func of _chatListeners)
+                func(json);
+        })
+    })
+
+}
 
 function AddChatListener(listener) {
     _chatListeners.push(listener);
